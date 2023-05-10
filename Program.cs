44edaac5db1d34efb4152a359ee25543b4f1d706
Ahmad Nabizada (1000113782) <NABIAS1@student.op.ac.nz>
2023-05-11 10:46:12 +1200

    a comment

--- Program.cs
+++ Program.cs
@@ -4,8 +4,11 @@ string ageAsString;
 int age;
 Console.WriteLine("write down you name: ");
 temp= Console.ReadLine();
-Console.WriteLine($"congrats your a member now{temp}");
+Console.WriteLine($"congrats your a member now {temp}");
 Console.WriteLine("Please enter your age: ");
 ageAsString = Console.ReadLine();
 age = Convert.ToInt32(ageAsString);
 Console.WriteLine($"{temp} you are {age} year old");
+
+Console.Writeline("this is just to test for merge conflicts and how to solve it");
+Console.WriteLine("we cant figure it out");
