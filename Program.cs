0ecc85a49a9402e0f1eb355c12005eb4e597da53
Ahmad Nabizada (1000113782) <NABIAS1@student.op.ac.nz>
2023-05-11 11:17:04 +1200

    it is working

--- Program.cs
+++ Program.cs
@@ -1,17 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-string temp;
-string ageAsString;
-int age;
-Console.WriteLine("write down you name: ");
-temp= Console.ReadLine();
-Console.WriteLine($"congrats your a member now {temp}");
-Console.WriteLine("Please enter your age: ");
-ageAsString = Console.ReadLine();
-age = Convert.ToInt32(ageAsString);
-Console.WriteLine($"{temp} you are {age} year old");
+using System.Diagnostics.CodeAnalysis;
 
-Console.Writeline("this is just to test for merge conflicts and how to solve it");
-Console.WriteLine("we cant figure it out");
+internal class Program
+    {
+
+        public static void Main()
+        {
+            string temp;
+  
+           
+            Console.WriteLine("write down you name: ");
+            temp = Console.ReadLine();
+            Console.WriteLine($"congrats your a member now {temp}");
+           
+            age();
+
+            Console.WriteLine("this is just to test for merge conflicts and how to solve it");
+            Console.WriteLine("we cant figure it out");
+        }
+        public static void age()
+        {
+        string ageAsString;
+        Console.WriteLine("Please enter your age: ");
+        ageAsString = Console.ReadLine();
+        int age = Convert.ToInt32(ageAsString);
+        Console.WriteLine($"you were born in {2023 - age}");
+       
+
+        
+        
+        }
+    }
 
 
 
@@ -27,7 +46,3 @@ Console.WriteLine("we cant figure it out");
 
 
 
-
-
-
-Console.WriteLine("this is a test for merge and see how close can we work on lines");
