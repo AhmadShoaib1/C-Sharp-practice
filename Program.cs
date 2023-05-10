d8b3485eefe53f0a044bda0f3b4e35a046a2c5b9
Krittapas Chinthumrucks (1000112540) <CHINK3@student.op.ac.nz>
2023-05-11 10:39:26 +1200

    Adding how old are you

--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 string temp;
-
+string ageAsString;
+int age;
 Console.WriteLine("write down you name: ");
 temp= Console.ReadLine();
-Console.WriteLine($"congrats your a member now{tmep}"s);
+Console.WriteLine($"congrats your a member now{temp}");
+Console.WriteLine("Please enter your age: ");
+ageAsString = Console.ReadLine();
+age = Convert.ToInt32(ageAsString);
+Console.WriteLine($"{temp} you are {age} year old");
